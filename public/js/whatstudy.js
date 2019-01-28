c555a93ceb1f61d624e9c0032249e6975bd6526b
Hans Bastiaan <h.bastiaan@windesheimflevoland.nl>
2019-01-28 23:09:39 +0100

    committ

--- public/js/whatstudy.js
+++ public/js/whatstudy.js
@@ -17,10 +17,9 @@ function addButtonActions() {
  */
 function fetchRooms() {
     var token = "259a7abd9185e783b791029864c2be6be6d17f18a1c094ec";
-    var myApi = new Api('GET', 'rooms/check/'+token, null);
+    var myApi = new Api('GET', 'rooms/check/' + token, null);
     myApi.execute(showRooms, ErrorRooms);
 }
-
 /*
  * show recieved Rooms
  */
