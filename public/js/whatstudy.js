cd9e543d133ee5957b656e2799c9a07ca1b1f621
Avinash B <thirtycarbonclocks@gmail.com>
2019-02-28 18:31:16 +0100

    US2 done... berichten worden getoond

--- public/js/whatstudy.js
+++ public/js/whatstudy.js
@@ -6,50 +6,81 @@ Vue.component('dropdown-item', {
     // template: "<a class='dropdown-item'>{{ room.name }}</a>"
 })
 
-const gatekeeperPage = { template: "<div>" +
-                                    "<h2>U bent nog niet ingelogd. I AM THE GATEKEEPER</h2>" +
-                                    "<p>U kunt deze site niet gebruiken zonder (als student of docent van Windesheim Flevoland) ingelogd te zijn.</p>" +
-                                    "</div>" }
+Vue.component('message', {
+    props: ['message'],
+    template: "<div><p>{{ message.user_id }} : {{ message.description }}</p></div>"
+})
 
-const roomPage = { template:  "<div>" +
-                              "<p>Welkom bij room {{ $route.params.id }}</p>" +
-                              "</div>",
-                    beforeRouteEnter: (to, from, next) => {
-                        var roomNumber = to.params.id;
-                        console.log("Connecting to room " + roomNumber);
-                        if (rooms === undefined) {
-                            router.push("/gatekeeper");
-                            redirectRoom = roomNumber;
-                        } else if (roomNumber >= (rooms.length + 1) || roomNumber <= 0) {
-                            router.push("/roomerror");
-                        } else {
-                            next();
-                        }
-                    },
-                    beforeRouteUpdate: (to, from, next) => {
-                        var roomNumber = to.params.id;
-                        console.log("Connecting to room " + roomNumber);
-                        if (rooms === undefined) {
-                            router.push("/gatekeeper?roomcallback=" + roomNumber);
-                        } else if (roomNumber >= (rooms.length + 1) || roomNumber <= 0) {
-                            router.push("/roomerror");
-                        } else {
-                            next();
-                        }
-                    },
-                }
+const welcomePage = {
+    template: "<div>" +
+        "<p>Welkom bij WhatStudy.</p>" +
+        "</div>",
+    beforeRouteEnter: (to, from, next) => {
+        console.log("Welcome to WhatStudy");
+        if (rooms === undefined) {
+            router.push("/gatekeeper");
+            redirect = "/";
+        } else {
+            next();
+        }
+    }
+}
 
-const roomErrorPage = { template:  "<div>" +
-                              "<p>Deze room kan niet geladen worden. Waarschijnlijk bestaat deze kamer niet.</p>" +
-                              "</div>"
-                        }
-                        
+const gatekeeperPage = {
+    template: "<div>" +
+        "<h2>U bent nog niet ingelogd. I AM THE GATEKEEPER</h2>" +
+        "<p>U kunt deze site niet gebruiken zonder (als student of docent van Windesheim Flevoland) ingelogd te zijn.</p>" +
+        "</div>"
+}
+
+const roomPage = {
+    template: "<div>" +
+    "<message v-for='message in this.$parent.messages' v-bind:message='message' v-bind:key='message.id'></message>" + 
+    "</div>",
+    beforeRouteEnter: (to, from, next) => {
+        var roomNumber = to.params.id;
+        console.log("Connecting to room " + roomNumber);
+        if (rooms === undefined) {
+            router.push("/gatekeeper");
+            redirect = "/room/" + roomNumber;
+        } else if (roomNumber >= (rooms.length + 1) || roomNumber <= 0) {
+            router.push("/roomerror");
+        } else {
+            next();
+            fetchMessages(userToken, roomNumber, 1);
+        }
+    },
+    beforeRouteUpdate: (to, from, next) => {
+        var roomNumber = to.params.id;
+        console.log("Connecting to room " + roomNumber);
+        if (rooms === undefined) {
+            router.push("/gatekeeper");
+            redirect = "/room/" + roomNumber;
+        } else if (roomNumber >= (rooms.length + 1) || roomNumber <= 0) {
+            router.push("/roomerror");
+        } else {
+            next();
+            fetchMessages(userToken, roomNumber, 1);
+        }
+    },
+}
+
+const roomErrorPage = {
+    template: "<div>" +
+        "<p>Deze room kan niet geladen worden. Waarschijnlijk bestaat deze kamer niet.</p>" +
+        "</div>"
+}
+
 const routes = [
-    { 
+    {
+        path: "/",
+        component: welcomePage
+    },
+    {
         path: "/gatekeeper",
         component: gatekeeperPage
     },
-    { 
+    {
         path: "/room/:id",
         component: roomPage
     },
@@ -64,24 +95,25 @@ const router = new VueRouter({
 })
 
 var app = new Vue({
+    data: { messages: false },
     router
 }).$mount('#app')
 
 
 var navBar = new Vue({
-    data: {rooms: false},
+    data: { rooms: false },
     router
 }).$mount('#navbar')
 
 var userToken;
 var rooms;
-var redirectRoom;
+var redirect;
 
 /**
  * Add actions to page buttons 
  */
 function addButtonActions() {
-    $(document).ready(function () { 
+    $(document).ready(function () {
         $("#btn-login").on("click", function () {
             login();
         });
@@ -110,9 +142,9 @@ function showRooms(response) {
     navBar.rooms = rooms;
     console.log(response);
 
-    if (redirectRoom !== undefined) {
-        router.push("/room/" + redirectRoom);
-        redirectRoom = undefined;
+    if (redirect !== undefined) {
+        router.push(redirect);
+        redirect = undefined;
     }
 
 }
@@ -125,20 +157,38 @@ function errorRooms(statusCode, errorMessage) {
     console.log(errorMessage);
 }
 
+function fetchMessages(token, roomID, page = 1) {
+    var myApi = new Api('GET', 'rooms/checkMessages/' + token.token + '/' + roomID + '?page=' + page, null);
+    myApi.execute(showMessages, errorMessages);
+}
+
+function showMessages(response) {
+    console.log(response);
+    app.messages = Array.from(response);
+}
+
+/*
+ * error fetching Messages
+ */
+function errorMessages(statusCode, errorMessage) {
+    console.log(statusCode);
+    console.log(errorMessage);
+}
+
 function tokenError(message) {
     // Do something with the message
     console.log("Token error:" + message);
 
     $('#login-fail-modal').modal('show');
 
-    $("#login-loading").hide();    
+    $("#login-loading").hide();
     $("#btn-login").show();
 }
 
 function tokenSuccess(token) {
     // Do something with the token
     userToken = token;
-    $("#login-loading").hide();    
+    $("#login-loading").hide();
     $("#btn-login").hide();
     $("#loggedin-text").html("Ingelogd als " + token.name.first + " " + token.name.last + " (" + token.id + ")");
     fetchRooms(userToken);
@@ -146,4 +196,5 @@ function tokenSuccess(token) {
 
 
 // initialize
+login();
 addButtonActions();
